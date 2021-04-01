871211005062803f6679d3d058cc3870d6269447
moummn <mmnstar@sina.com>
2021-04-01 13:31:45 +0800

    20210401

---
+++ GSMT/MainWindow.xaml.vb
@@ -0,0 +1,11 @@
+﻿Class MainWindow
+    Private Sub Button1_Click(sender As Object, e As RoutedEventArgs) Handles Button1.Click
+        Dim pg As Page = New Page1
+        Content1.Content = pg.Content
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As RoutedEventArgs) Handles Button2.Click
+        Dim pg As Page = New Page2
+        Content1.Content = pg.Content
+    End Sub
+End Class
